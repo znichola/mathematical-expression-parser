dccf726fda79ed17f564b276fb84662ff84b61de
caf3d00d <leetpwnsroot@gmail.com>
2022-12-25 20:54:17 +0100

    Fixing a segmentation fault while passing a NULL string It is possible to get a 'segmentation fault core dup' as the function parse_args didn't check for the string if it's valid or NULL, therefore a NULL string pointer was being dereferenced. It should be checked before attemptint to parse the expression.

--- args.c
+++ args.c
@@ -2,7 +2,8 @@
 
 static int	check_args(int argc, char **argv)
 {
-	if (argc < 2 || argc > 4)
+    // I think you mean 3 instead of 4 :D
+	if (argc < 2 || argc > 3)
 		return (-1);
 	if (argc == 3 && strcmp(argv[1], "-p") != 0)
 		return (-1);
@@ -26,5 +27,26 @@ int	parse_args(int argc, char **argv, char **expr, int *action)
 		*expr = argv[2];
 		*action = PRINT_TREE;
 	}
+    if (expr == NULL || *expr == NULL || **expr == '\0')
+    {
+        printf("Error: Expression is NULL.\n");
+        return -1;
+    }
 	return (0);
 }
+
+/*
+ * The getopt function could be used to parse options. -> maybe in future.
+ while ((opt = getopt(argc, argv, "p")) != -1)
+	{
+		switch (opt) {
+			case 'p':
+				*action = PRINT_TREE;
+				break;
+			// check for invalid options
+			default:
+				printf("usage: ./eval <expr> or ./eval -p <expr>\n");
+				return -1;
+		}
+	}
+ */
